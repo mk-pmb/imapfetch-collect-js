363bc0843d93bf817e6f38de5da52c6dae51b7ec
mk-pmb <mk-pmb@users.noreply.github.com>
2017-06-16 02:25:32 +0200

    v0.1.0

---
+++ README.md
@@ -0,0 +1,112 @@
+﻿
+<!--#echo json="package.json" key="name" underline="=" -->
+imapfetch-collect
+=================
+<!--/#echo -->
+
+<!--#echo json="package.json" key="description" -->
+Simplified abstraction for the `fetch` method of the `imap` module. Get a
+callback instead of streams within streams.
+<!--/#echo -->
+
+
+API
+---
+
+This module exports one function:
+
+### imapFetchCollect(fetcher, [opts,] whenFetched)
+
+* `fetcher`: The `ImapFetch` object returned by the `imap` module's `fetch`
+  method.
+* `opts`: Optional options object, see below.
+* `whenFetched`: Your callback (nodeback).
+  It will receive two arguments: `(err, msgs)`,
+  where `msgs` is an Array of messages that were received successfully
+  ([example data](docs/msgs-array-example-01.json)),
+  and `err` is the first error that was encountered,
+  or some false-y value on success.
+
+If you need progress information, you can add your own event listeners to
+the `fetcher`. `imapFetchCollect`'s event handlers shouldn't interfere
+with others.
+
+
+Options
+-------
+
+* `translateHeaderNames`:
+  Whether and how to rewrite header field names,
+  using the `transkey` module.
+  * `false`, `null`, `undefined` (default): Don't.
+  * a Function: Custom synchronous translater function.
+  * `"dash2camel"`: Translate to camelCase, e.g. `from`, `to`,
+    `xOriginalTo`, `messageId`, `contentTransferEncoding`
+
+* `maxDecodeBytes`:
+  How much of a text buffer to decode automatically. Can be
+  * any false-y value (e.g. `0`): Use some default value of a few megabytes.
+  * a positive Number: Up to that many bytes.
+  * `true`: Decode ALL the text.
+
+* `simpleUniqueHeaders`:
+  Whether to unpack header value arrays that contain only one value.
+  Boolean, default: `true`
+
+
+Usage
+-----
+
+<!--!#include file="test/usage.js" start="  //#u" stop="  //#r"
+  outdent="  " code="javascript" -->
+```javascript
+var ImapConnection = require('imap'),
+  imapFetchCollect = require('imapfetch-collect');
+
+function onMailFetched(err, msgs) {
+  if (err) { throw err; }
+  console.log('fetched', msgs.length, 'message(s)');
+  var msg1 = msgs[0];
+  console.log('first mail headers:', Object.keys(msg1.rawHeaders));
+  console.log('first body:', msg1.bodies[0].text);
+}
+
+function checkMail() {
+  // … login, search, …
+  function onSearchSuccess(foundUIDs) {
+    var fetcher = imapConn.fetch(foundUIDs, fetchOpts);
+    imapFetchCollect(fetcher, onMailFetched);
+  }
+}
+```
+<!--/include-->
+
+
+<!--#toc stop="scan" -->
+
+
+Simplification drawbacks
+------------------------
+
+* In case of multiple errors within the same fetch attempt,
+  all but one are silently ignored.
+* All messages are buffered into memory. You don't get a chance to ignore
+  some message body based on the message's size or headers.
+
+
+Known issues
+------------
+
+* needs more/better tests and docs
+
+
+
+
+&nbsp;
+
+
+License
+-------
+<!--#echo json="package.json" key=".license" -->
+ISC
+<!--/#echo -->
---
+++ fetch.js
@@ -0,0 +1,110 @@
+﻿/*jslint indent: 2, maxlen: 80, continue: false, unparam: false, node: true */
+/* -*- tab-width: 2 -*- */
+'use strict';
+
+module.exports = (function () {
+  var EX, parseHeader = require('imap').parseHeader,
+    posInf = Number.POSITIVE_INFINITY,
+    transkey = require('transkey'),
+    headerEol = '\r\n',   // https://github.com/mscdex/node-imap/issues/621
+    collectStream = require('collect-stream');
+
+  function ifFun(x, d) { return ((typeof x) === 'function' ? x : d); }
+  function ifTrue(x, t) { return (x === true ? t : x); }
+  function limitSlice(x, l) { return (x.length > l ? x.slice(0, l) : x); }
+
+  EX = function imapFetchCollect(fetcher, opts, whenFetched) {
+    if (!ifFun(whenFetched)) {
+      if (ifFun(opts)) {
+        whenFetched = opts;
+        opts = false;
+      }
+    }
+    var msgs = [], whyFailed = null, dfOpt = EX.defaultOpts,
+      maxDecodeBytes = (+ifTrue(opts.maxDecodeBytes, posInf)
+        || dfOpt.maxDecodeBytes);
+
+    msgs.byUID = {};
+    function addMsgByUID(msg) {
+      var uid = msg.uid;
+      if (uid === +uid) { msgs.byUID[uid] = msg; }
+    }
+
+    function fail(why) {
+      if (whyFailed) { return; }
+      whyFailed = why;
+    }
+    fetcher.on('error', fail);
+
+    function recvMsg(reader, seqno) {
+      if (whyFailed) { return; }
+      var rawHeaders = [], msg = { seqno: seqno,
+        headers: false,
+        getRawHeaders: function getRawHeaders() { return rawHeaders; },
+        bodies: [],
+        };
+
+      function updAttr(attr) {
+        Object.assign(msg, attr);
+      }
+      reader.on('attributes', updAttr);
+
+      function startRecvBody(bodyStream, meta) {
+        if (whyFailed) { return; }
+        function saveRecvdBody(err, bodyBuffer) {
+          if (whyFailed) { return; }
+          if (err) { fail(err); }
+          var bodyInfo = meta, isHeader = (meta.which === 'HEADER'), tx;
+          if (isHeader) {
+            rawHeaders.push(bodyInfo);
+          } else {
+            bodyInfo.getRawBuffer = function () { return bodyBuffer; };
+            msg.bodies.push(bodyInfo);
+          }
+          tx = limitSlice(bodyBuffer, maxDecodeBytes).toString('UTF-8');
+          bodyInfo.text = tx;
+          bodyInfo.ucs2_length = tx.length;
+        }
+        collectStream(bodyStream, saveRecvdBody);
+      }
+      reader.on('body', startRecvBody);
+
+      function finishMsg() {
+        msgs.push(msg);
+        addMsgByUID(msg);
+        var hdr = EX.parseCombinedHeaders(rawHeaders,
+          opts.translateHeaderNames);
+        if (opts.simpleUniqueHeaders !== false) {
+          hdr = transkey(function (k, v) {
+            if (v.join && (v.length === 1)) { v = v[0]; }
+            return [k, v];
+          }, hdr);
+        }
+        msg.headers = hdr;
+      }
+      reader.on('end', finishMsg);
+    }
+    fetcher.on('message', recvMsg);
+
+    fetcher.on('end', function fetcherDone() {
+      whenFetched(whyFailed, msgs);
+    });
+  };
+
+
+  EX.defaultOpts = {
+    maxDecodeBytes: 16 * Math.pow(1024, 2),
+  };
+
+
+  EX.parseCombinedHeaders = function (rawHeaders, translate) {
+    rawHeaders = rawHeaders.map(function (h) { return h.text; }
+      ).join('\n').replace(/\r*\n[\r\n]*/g, headerEol);
+    var hdr = parseHeader(rawHeaders);
+    if (translate) { hdr = transkey(translate, hdr); }
+    return hdr;
+  };
+
+
+  return EX;
+}());
